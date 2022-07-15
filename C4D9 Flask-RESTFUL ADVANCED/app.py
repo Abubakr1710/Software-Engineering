605a7b6a2fc3d1827988b96d89b27fda528d9b99
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-07-15 04:43:24 -0700

    testing

--- C4D9 Flask-RESTFUL ADVANCED/app.py
+++ C4D9 Flask-RESTFUL ADVANCED/app.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from flask import Flask, request, Response, render_template
 from utils.db import db_init, db
 from werkzeug.utils import secure_filename
@@ -13,6 +14,10 @@ db_init(app)
 def index():
     return render_template('index.html')
 
+@app.route("/first") 
+def test():
+    return render_template('test.html')
+
 @app.route('/upload', methods=['POST'])
 def upload():
     pic = request.files['pic']
@@ -26,13 +31,22 @@ def upload():
     db.session.add(img)
     db.session.commit()
 
-    return 'Image uploaded', 200
+    return Response(test())
 
-@app.route('/int/<id>')
+@app.route('/<int:id>')
 def get_img(id):
-    img = Img.query.get(id).first()
+    img = Img.query.filter_by(id=id).first()
     if not img:
         return 'Image not found', 404
 
-    return Response(img.img, mimetype=img.mimetype)   
+    return Response(img.img, mimetype=img.mimetype)
+
+
+@app.route('/<string:name>')
+def get_img_by_name(name):
+    img = Img.query.filter_by(name=name).first()
+    if not img:
+        return 'Image not found', 404
+
+    return Response(img.img, mimetype=img.mimetype)      
     
